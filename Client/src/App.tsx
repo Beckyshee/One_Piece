24a3ba8d15ac732fa73e8c7172b8e611b9a6a0d3
Rebecca Kariuki <97875510+Beckyshee@users.noreply.github.com>
2024-05-22 16:51:35 +0300

    landing

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,19 +2,16 @@
 import './App.scss'
 import NotFound from './Pages/404/NotFound';
 import Landing from './Pages/landing/Landing'
-import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App: React.FC=()=> {
 
 
   return (
     <Router>
-      {/* <div >
-        <Landing/>
-   </div> */}
+      
       <Routes>
         <Route path="/" element={<Landing />} />
-        {/* Add other routes here */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
