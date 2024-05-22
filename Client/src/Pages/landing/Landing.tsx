989be218f740dd45b8cd692cee792a3db9f31523
Rebecca Kariuki <97875510+Beckyshee@users.noreply.github.com>
2024-05-22 15:48:34 +0300

    navbar

--- Client/src/Pages/landing/Landing.tsx
+++ Client/src/Pages/landing/Landing.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+
+import Navbar from "../../components/Navbar";
+import zoro from '../../assets/zoro1.jpg'
+import ace from '../../assets/portgas.jpg'
+import shanks from '../../assets/shanks1.jpg'
+import "./landing.scss";
 
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <div className="landing-container">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="landing-content">
+        <h1 className="title">Anime One Piece!!</h1>
+        <p className="description">
+          The story follows the adventures of Monkey D. Luffy and his crew, the
+          Straw Hat Pirates, where he explores the Grand Line in search of the
+          mythical treasure...
+        </p>
+        <button className="watch-button">Watch now</button>
+        <div className="rating-genre">
+          <div className="rating">
+            <span>Rating:</span> ⭐⭐⭐⭐⭐
+          </div>
+          <div className="genre">
+            <span>Genre:</span> Shonen manga, Action manga, Comedy, Fantasy
+          </div>
+        </div>
+        <div className="thumbnails">
+          <img src={zoro} alt="Thumbnail 1" />
+          <img src={ace} alt="Thumbnail 2" />
+          <img src={shanks} alt="Thumbnail 3" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Landing
+export default Landing;
--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -1,9 +1,26 @@
-import '/navbar.scss'
+import "./navbar.scss";
+import logo from "../assets/op.jpg";import { FaSearch, FaBell } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className='container'>Navbar</div>
-  )
-}
+    <div className="container">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Episodes</a>
+        <a href="/">Characters</a>
+        <a href="/">Wiki</a>
+        <a href="/">Merch</a>
+      </div>
+      <div className="nav-icons">
+        <FaSearch className="icon" />
+        <FaBell className="icon" />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
